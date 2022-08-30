31bee3562a71fb668f3979cb38c4ac727af79f7f
ChimaCuma <oleg.bragin.01@gmail.com>
2022-08-30 15:55:12 +0300

    added username header

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,23 +1,44 @@
-import { Formik } from "formik"
-import * as Yup from "yup"
-
+import { Formik } from "formik";
+import * as Yup from "yup";
+import React, { useState } from "react";
 
 const Register = () => {
     const schema = Yup.object().shape({
-        username: Yup.string().required("")
+        username: Yup.string()
+            .required("enter username")
             .min(6, "the username should have 8 charcters"),
-        password: Yup.string().required("")
+        password: Yup.string()
+            .required("enter password")
             .min(8, "the password should have 8 charcters"),
-        password2: Yup.string().required("")
+        password2: Yup.string()
+            .required("enter password")
             .min(8, "the password should have 8 charcters")
-            .oneOf([Yup.ref('password'), null], 'Passwords must match')
-    })
+            .oneOf([Yup.ref("password"), null], "Passwords must match"),
+    });
+
+    const [userName, setUserName] = useState("");
     return (
         <div>
             <h1>login</h1>
-            <Formik validationSchema={schema} initialValues={{ username: '', password: '', password2: '', checkbox: false }} onSubmit={(values) => alert("hello" + JSON.stringify(values))}  >
-                {({ handleSubmit, handleChange, handleBlur, values, errors, touched }) => (
-                    <form onSubmit={handleSubmit} noValidate >
+            <Formik
+                validationSchema={schema}
+                initialValues={{
+                    username: "",
+                    password: "",
+                    password2: "",
+                    checkbox: false,
+                }}
+                onSubmit={(values) => alert("hello" + JSON.stringify(values))}
+            >
+                {({
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    values,
+                    errors,
+                    touched,
+                }) => (
+                    <form onSubmit={handleSubmit} noValidate>
                         <input
                             type="text"
                             name="username"
@@ -48,21 +69,28 @@ const Register = () => {
                         <p>{errors.password2 && touched.password2 && errors.password2}</p>
 
                         <label>
-                            <input type="checkbox"
-                                name="checkbox"
-                                onChange={handleChange} />
-                            i agree to the terms
+                            <input type="checkbox" name="checkbox" onChange={handleChange} />i
+                            agree to the terms
                         </label>
                         <br></br>
 
-                        {values.checkbox ? (<button type="submit">login</button>) : (<div></div>)}
-
+                        {values.checkbox ? (
+                            <button
+                                type="submit"
+                                onClick={() => setUserName(values.username)}
+                            >
+                                login
+                            </button>
+                        ) : (
+                            <div></div>
+                        )}
                     </form>
                 )}
-
+                
             </Formik>
-        </div >
-    )
-}
+            <h1>Welcome, {userName}</h1>
+        </div>
+    );
+};
 
-export default Register
+export default Register;
