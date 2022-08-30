e986675da2bdf49c918a3b311e5b7359dc925ab7
rani <ranilang@outlook.com>
2022-08-30 15:17:33 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
 import Contact from './Contact';
+import Register from "./Register";
 
 function App() {
   return (
     <div className="App">
+      <Register></Register>
       <Contact></Contact>
     </div>
   );
