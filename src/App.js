18718cba9f401bc254699c245224fc19a10cdff2
ChimaCuma <oleg.bragin.01@gmail.com>
2022-08-30 14:37:53 +0300

    Contact added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from "react";
 import './App.css';
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
-
+      <Contact></Contact>
     </div>
   );
 }
